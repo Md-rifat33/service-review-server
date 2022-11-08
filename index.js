059199638b7a867fbf41b4b033fee0b8cf0dabe2
Md-rifat33 <ultimaterifat@yahoo.com>
2022-11-09 03:03:06 +0600

    specific data create completed

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -32,6 +32,13 @@ async function run() {
         const cursor = cobblerServices.find(query)
         const services = await cursor.toArray()
         res.send(services)
+      })
+
+      app.get('/services/:id', async (req, res) => {
+        const id = req.params.id
+        const query = { _id: ObjectId(id) }
+        const user = await cobblerServices.findOne(query)
+        res.send(user)
       })
     })
   } finally {
